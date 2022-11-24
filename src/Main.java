22bd7c4750ea39a9250f8ac81a3433cc96f8fee5
KasiaH11 <katarzyna.haladyniak.praca@onet.pl>
2022-11-24 13:38:51 +0100

    if & else

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,18 @@
-
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Podaj pierwsza cyfre");
         Scanner scanner=new Scanner(System.in);
-        System.out.println("Podaj swoje imię");
-        String name=scanner.nextLine();
-        System.out.println("Podaj swój wiek");
-        int age= scanner.nextInt();
-        System.out.println("Czesc mam na imie "+name+" mam " +age+" lat");
+        int number1=scanner.nextInt();
+        System.out.println("Podaj druga cyfre");
+        int number2=scanner.nextInt();
+        if(number1>number2&&number1<5){
+            System.out.println(number1+ " jest mniejsza od 5");
+        }
+        else{System.out.println("Nie wiem co to jest :)");
+
+        }
     }
 }
+
